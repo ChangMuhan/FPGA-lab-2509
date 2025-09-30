45b7a95c64b6cf494512910ff5f859a3ec387248
MUHAN CHANG <1230006884@student.must.edu.mo>
2025-09-30 11:24:03 +0800

    Implement full adder module in Verilog

--- lab01/full_adder/full_adder.v
+++ lab01/full_adder/full_adder.v
@@ -1,1 +1,13 @@
+module full_adder(
+	input wire addend,
+	input wire augend,
+	input wire c_in,
+	output wire c_out,
+	output wire sum
+);
 
+assign sum = addend^augend^c_in;
+
+assign c_out = ((addend^augend)&c_in) | (addend&augend);
+
+endmodule
