1450cd9b7704b2338f8661a5bd68b6df3690de37
MUHAN CHANG <1230006884@student.must.edu.mo>
2025-10-23 16:06:46 +0800

    Add testbench for PLL module

---
+++ lab03/testbench/tb_pll.v
@@ -0,0 +1,25 @@
+`timescale 1ns/1ps
+
+module tb_pll();
+
+reg artificial_sys_clk;
+reg artificial_rst_n;
+
+wire recorder_vga_clk;
+
+initial begin
+	artificial_sys_clk <= 1'b1;
+	artificial_rst_n <= 1'b0;
+	#200
+	artificial_rst_n <= 1'b1;
+end
+
+always #10 artificial_sys_clk = ~artificial_sys_clk;
+
+pll pll_inst(
+	.sys_clk(artificial_sys_clk),
+	.sys_rst_n(artificial_rst_n),
+	.vga_clk(recorder_vga_clk)
+);
+
+endmodule
